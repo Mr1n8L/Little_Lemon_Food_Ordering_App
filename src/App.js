ab0b8927522c5b807cb3f6fd5b41ce740d4de78b
Mr1n8L <mrinaljani1822@gmail.com>
2024-03-23 19:00:32 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Nav from "./Components/Nav";
+import PageFooter from "./Components/PageFooter";
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <PageFooter />
+    </div>
+  );
+}
+
+export default App;
